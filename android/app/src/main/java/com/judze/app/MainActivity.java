667d84fcfac8a772202ac6f3296eb9b8b1869f5d
Pavels <dnicer3@gmail.com>
2024-06-07 16:03:40 +0300

    Added iOS and Android

---
+++ android/app/src/main/java/com/judze/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.judze.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/components/widgets/Menu/Grid.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+	interface MenuItem {
+		icon: string;
+		label: string;
+		link: string;
+	}
+</script>
+
+<script lang="ts">
+	export let items: MenuItem[];
+</script>
+
+<div>
+	{#each items as item}
+		<a href="{ item.link }">
+			<span>{ item.label }</span>
+		</a>
+	{/each}
+</div>
+
+<style lang="scss">
+	
+</style>
